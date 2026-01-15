f39117b64bc428ac43686c97c8b394c97e46fb8d
Musawenkosistar <musawenkosi2504@gmail.com>
2026-01-14 21:26:37 -0800

    LibraryProject

--- Introduction_to_Django/LibraryProject/bookshelf/apps.py
+++ Introduction_to_Django/LibraryProject/bookshelf/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
 from django.contrib import admin
-from .models import Book
 
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
