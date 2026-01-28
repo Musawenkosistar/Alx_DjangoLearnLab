7ecc26c0fc3e10fc6fd2d61b7cfee060359ec1a7
Musawenkosistar <musawenkosi2504@gmail.com>
2026-01-28 02:26:33 -0800

    api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
@@ -11,4 +12,7 @@ urlpatterns = [
 
     # Routes for CRUD operations via ViewSet
     path('', include(router.urls)),
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/', include('api.urls')),
 ]
