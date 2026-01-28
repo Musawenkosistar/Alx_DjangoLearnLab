27f85bf3b8afb56f100cf833eaa190b9095a9b09
Musawenkosistar <musawenkosi2504@gmail.com>
2026-01-28 04:18:05 -0800

    django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.filter(name="Chinua Achebe").first()
-if author:
-    books_by_author = Book.objects.filter(author=author)
-    print("Books by author:", books_by_author)
-else:
-    print("Author not found")
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print("Books by author:", books_by_author)
 
 # List all books in a library
-library = Library.objects.filter(name="Central Library").first()
-if library:
-    books_in_library = library.books.all()
-    print("Books in library:", books_in_library)
-else:
-    print("Library not found")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print("Books in library:", books_in_library)
 
 # Retrieve the librarian for a library
-if library:
-    librarian = Librarian.objects.filter(library=library).first()
-    print("Librarian:", librarian)
+librarian = Librarian.objects.get(library=library)
+print("Librarian:", librarian)
