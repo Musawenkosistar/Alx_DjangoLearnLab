dc127c2bb17cea58225dbc7a24968e4c8949e896
Musawenkosistar <musawenkosi2504@gmail.com>
2026-02-10 04:32:22 -0800

    advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+
+
 class BookForm(forms.ModelForm):
+    
     class Meta:
         model = Book
-        fields = ['title']
+        fields = ['title', 'author', 'publication_year']
