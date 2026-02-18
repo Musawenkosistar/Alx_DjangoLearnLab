5a54dc6916682311bd83c4b33031bf02bbf4f48f
Musawenkosistar <musawenkosi2504@gmail.com>
2026-02-18 04:21:04 -0800

    api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,17 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def feed_view(request):
-    followed_users = request.user.following.all()
+    following_users = request.user.following.all()
+
     posts = Post.objects.filter(
-        author__in=followed_users
+        author__in=following_users
     ).order_by('-created_at')
 
     serializer = PostSerializer(posts, many=True)
